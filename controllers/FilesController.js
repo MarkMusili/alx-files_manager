023f12e0c62e1c037403452f3d571bed1cf46832
MarkMusili <musiliyrn@gmail.com>
2024-05-15 15:44:35 +0300

    File controller

---
+++ controllers/FilesController.js
@@ -0,0 +1,58 @@
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+const { ObjectID } = require('mongodb');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+exports.postUpload = async (req, res) => {
+    const token = req.get('X-Token');
+    const userId = await redisClient.get(`auth_${token}`);
+    const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ _id: ObjectID(userId) });
+    if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const { name, type, parentId, isPublic, data } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ error: 'Missing name' });
+    }
+
+    if (!type ) {
+        return res.status(400).json({ error: 'Missing type' });
+    }
+    if (!data && type !== folder) {
+        return res.status(400).json({ error: 'Missing data' });
+    }
+    if (parentId) {
+        const parent = await dbClient.client.db(dbClient.database).collection('files').findOne({ _id: ObjectID(parentId) });
+        if (!parent) {
+            return res.status(400).json({ error: 'Parent not found' });
+        }
+        if (parent.type !== 'folder') {
+            return res.status(400).json({ error: 'Parent is not a folder' });
+        }
+    }
+
+    const fileData = {
+        userId: user._id,
+        name: name,
+        type: type,
+        parentId: parentId || 0,
+        isPublic: isPublic || false,
+    };
+
+    if (type !== 'folder') {
+        const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true });
+        }
+        const filePath = path.join(folderPath, uuidv4());
+        fs.writeFileSync(filePath, data, 'base64');
+        fileData.localPath = filePath;
+    }
+
+    const file = await dbClient.client.db(dbClient.database).collection('files').insertOne(fileData);
+    return res.status(201).json({ id: file.insertedId, ...fileData});
+};
